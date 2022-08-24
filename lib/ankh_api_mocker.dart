b317ebaab3e01300f5841e66955b7edc44e095fe
unknown <bruneltesse@gmail.com>
2022-08-24 15:33:26 +0100

    Moving forward

--- lib/ankh_api_mocker.dart
+++ lib/ankh_api_mocker.dart
@@ -1,31 +1,152 @@
+// ignore_for_file: unused_element
+
 library ankh_api_mocker;
 
 import 'dart:math' as math;
 
+/*
+Author : Afrographix
+This package helps you to mock an API response
+
+for example
+
+-----
+Input
+-----
+{
+  int idUser;
+  bool isOnline;
+  double height;
+  String fullName;
+}
+
+------
+Output
+------
+{
+  idUser:1,
+  isOnline:false,
+  height:1.45,
+  fullName:"Sekhmet"
+}
+*/
+
 /// A Calculator.
 class AnkhAPIMocker {
-  int randomIntGenerator(int max) {
+  int _randomIntGenerator(int max) {
     var random = math.Random();
     return random.nextInt(max);
   }
 
-  double randomDoubleGenerator() {
+  double _randomDoubleGenerator() {
     var random = math.Random();
     return double.parse((random.nextDouble() * 100).toStringAsFixed(2));
   }
 
-  bool randomBoolGenerator() {
+  bool _randomBoolGenerator() {
     var date = DateTime.now().millisecondsSinceEpoch;
     return date % 2 == 0;
   }
 
-  String randomStringGenerator() {
-    if (randomBoolGenerator()) return "Sekhmet";
+  String _randomStringGenerator() {
+    if (_randomBoolGenerator()) return "Sekhmet";
     List<String> chars = ["Ankh", "Maat", "Amon", "Afro"];
     String suffix = "";
-    suffix += chars[randomIntGenerator(chars.length - 1)];
+    suffix += chars[_randomIntGenerator(chars.length - 1)];
     return "Sekhmet-" + suffix;
   }
+
+  String _removeAllLargeSpaces(String fieldSchema) {
+    fieldSchema = fieldSchema.trim();
+    fieldSchema = fieldSchema.replaceAllMapped(RegExp(r'\s+'), (match) {
+      return " ";
+    });
+    return fieldSchema;
+  }
+
+  bool _validFieldsSchema(String fieldSchema) {
+    final requiredFieldSchema =
+        RegExp(r'{\s*((int|bool|String|double)\??\s+\S+\s*;\s+)+\s*}');
+    return requiredFieldSchema.hasMatch(fieldSchema);
+  }
+
+  String _extractField(String fieldSchema) {
+    final regExp = RegExp(r'{(.+)}');
+    final match = regExp.firstMatch(fieldSchema);
+    return match?.group(1) as String;
+  }
+
+  String _generateRandom(String dataType) {
+    switch (dataType) {
+      case "int":
+        {
+          return "${_randomIntGenerator(10000)}";
+        }
+
+      case "int?":
+        {
+          return "${_randomIntGenerator(10000)}";
+        }
+
+      case "String":
+        {
+          return _randomStringGenerator();
+        }
+
+      case "String?":
+        {
+          return _randomStringGenerator();
+        }
+
+      case "double":
+        {
+          return "${_randomDoubleGenerator()}";
+        }
+
+      case "double?":
+        {
+          return "${_randomDoubleGenerator()}";
+        }
+
+      case "bool":
+        {
+          return "${_randomBoolGenerator()}";
+        }
+
+      case "bool?":
+        {
+          return "${_randomBoolGenerator()}";
+        }
+
+      default:
+        {
+          return "-1";
+        }
+    }
+  }
+
+  List<Field> parseFieldStringToArray(String fieldsString) {
+    List<Field> fields = [];
+
+    return fields;
+  }
+
+  dynamic generateData(String fieldSchema) {
+    fieldSchema = _removeAllLargeSpaces(fieldSchema);
+    if (_validFieldsSchema(fieldSchema)) {
+      String fields = _extractField(fieldSchema);
+      print(fields);
+    } else {
+      throw Exception("Invalid Field Schema,Please check the documentation");
+    }
+  }
+}
+
+class Field {
+  String dataType;
+  String data;
+
+  Field({required this.dataType, required this.data});
 }
 
 class User {
@@ -40,8 +161,9 @@ class User {
   });
 
   static String AnkhAPIMockerInit = """{
-    int idUser;
-    String name;
-    String email;
-  }""";
+  int idUser;
+  bool isOnline;
+  double height;
+  String fullName;
+}""";
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,6 @@ import 'dart:developer';
 import 'package:ankh_api_mocker/ankh_api_mocker.dart';
 
 void main() {
-  User user1 = User(
-    idUser: 1,
-    email: "bruneltesse@gmailcom",
-    name: 'Afrographix',
-  );
-
   AnkhAPIMocker mocker = AnkhAPIMocker();
-  print(mocker.randomStringGenerator());
-
-  // print(User.AnkhAPIMockerInit);
-
-  // inspect(user1.)
+  mocker.generateData(User.AnkhAPIMockerInit);
 }
